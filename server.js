518f7dea22b023ca6a91570e557c49a78d042c36
Lukas Li <lukas.li.album@gmail.com>
2025-07-20 09:53:46 -0400

    removed node modules

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
